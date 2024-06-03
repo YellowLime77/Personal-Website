9c6957ddf8c9049ccaa64677a6792b40508e428a
YellowLime77 (Matthew Yang) <matthewyanglele@gmail.com>
2024-06-02 22:52:38 -0400

    temp website update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="background-div h-screen w-screen">
+      <div className='h-full w-full backdrop-blur-div flex justify-center items-center'>
+        <div className="rounded-2xl z-10 shadow-lg bg-transparent/75 border w-fit m-[50px] px-[100px] py-[80px]">
+          <h1 className="font-bold">Matthew Yang</h1>
+
+          <p>Just a temporary website</p>
+
+          <p>In the meantime, check out <a href="https://vocaloidle.matthewyang.tech/">Vocaloidle: A Vocaloid Heardle</a></p>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
