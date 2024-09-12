07120158afbe4fb84524ff8eb2f455b03969a17f
Matthew <matthewyanglele@gmail.com>
2024-09-12 10:34:34 -0400

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,7 +6,7 @@ function App() {
     <div className="background-div h-screen w-screen">
       <div className='h-full w-full backdrop-blur-div flex justify-center items-center'>
         <div className="rounded-2xl z-10 shadow-lg bg-transparent/75 border w-fit m-[50px] px-[100px] py-[80px]">
-          <h1 className="font-bold">Matthew Yang</h1>
+          <h1 className="font-bold text-slate-800">Matthew Yang</h1>
 
           <p>Just a temporary website</p>
 
